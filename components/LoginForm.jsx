521346e879d881d5365db99c28e88c5bda1047ea
Nikhil <nikhildmagar@gmail.com>
2024-01-08 01:18:22 +0530

    fixed

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -61,7 +61,8 @@ export default function LoginForm() {
                             <p className="font-medium text-white-700 pb-2">Password</p>
                             <input onChange={(e)=>setPassword(e.target.value)} id="password" name="password" type="password" className="w-full py-2 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow text-black" placeholder="Enter your password" />
                         </label>
-                        
+                        <div className="pb-2"></div>
+
                         <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                             <span>Login</span>
                         </button>
